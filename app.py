f4251c3e0c2f1bb129061ef943426bce8407c3f3
celim <chaieun.lim@gmail.com>
2022-10-31 12:01:16 +0900

    1st

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+st.title("My streamlit")
+
+st.write("fantasic !!!")
+
+st.sidebar.title("Side")
+st.sidebar.radio("Select", ["A","B"])
+st.sidebar.selectbox('Select Page',['Home','Prediction'])
+
+st.camera_input("Camera")
+
+rand=np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
+
+df= pd.DataFrame(np.random.randn(10, 2),columns=['x', 'y'])
+st.line_chart(df)
+st.bar_chart(df)
+
+btn = st.button("Press for progress")
+
+if btn:
+    progrss = st.progress(0)
+
+    with st.spinner('Wait for it...'):
+        for i in range(10):
+            # print(i)
+            progrss.progress((i+1) * 10)
+            time.sleep(0.1)
+        
+
+st.info("Done")
+
+
+
+
+
+
+
+    
+
+
